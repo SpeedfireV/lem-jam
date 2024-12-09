549db2b7bcdd147e86510f82c0ab53fc56dbb477
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-12-09 19:06:48 +0100

    added player to scenes manager

--- managers/scenes_manager.gd
+++ managers/scenes_manager.gd
@@ -2,3 +2,4 @@ extends Node
 
 
 var minigames_node: Node
+var player: Player
--- src/player/player.gd
+++ src/player/player.gd
@@ -4,6 +4,9 @@ const SPEED = 500.0
 var direction: Vector2 = Vector2.ZERO
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
+func _ready():
+	ScenesManager.player = self
+
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
