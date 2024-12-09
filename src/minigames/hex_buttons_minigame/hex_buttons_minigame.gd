20ba988145109457346778cd32cecab5a0c2879a
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-12-09 21:46:19 +0100

    hex done

--- src/minigames/hex_buttons_minigame/hex_buttons_minigame.gd
+++ src/minigames/hex_buttons_minigame/hex_buttons_minigame.gd
@@ -1,6 +1,8 @@
 class_name HexButtonsMinigame extends Control
 
 
+signal finished(win: bool)
+
 var buttons: Array[Button] = []
 
 var colors: Array[Color] = [Color(0xff0000ff), Color(0x0000ffff), Color(0x00ff00ff), Color(0xffff00ff), Color(0xffa500ff), Color(0x800080ff), Color(0xff69b4ff), Color(0x40e090ff), Color(0x87ceebff)]
@@ -15,11 +17,24 @@ var colors: Array[Color] = [Color(0xff0000ff), Color(0x0000ffff), Color(0x00ff00
 @onready var turquse_button: Button = $VBoxContainer/HBoxContainer/GridContainer/Button8
 @onready var aqua_button: Button = $VBoxContainer/HBoxContainer/GridContainer/Button9
 
-@onready var answer_label = $VBoxContainer/AnswerLabel
+@onready var answer_label: Label = $VBoxContainer/AnswerLabel
+@onready var accept_button: Button = $VBoxContainer/HBoxContainer/AcceptButton
+@onready var cancel_button: Button = $VBoxContainer/HBoxContainer/CancelButton
+
+var entered_sequence: Array[int] = []
 
 
-var answer: String = ""
 
+var configurations: Dictionary = {
+	"123" : [2,2,3,4,5,6],
+	"132" : [6,7,8,2,1,4],
+	"213" : [3,4,5,6,7,2],
+	"231" : [0,2,1,5,2,7],
+	"312" : [8,7,2,4,5,0],
+	"321" : [5,4,3,2,6,8]
+}
+
+var selected_configuration = configurations.keys().pick_random()
 
 func _ready():
 	buttons.append(red_button)
@@ -31,17 +46,43 @@ func _ready():
 	buttons.append(pink_button)
 	buttons.append(turquse_button)
 	buttons.append(aqua_button)
+	for i in range(3):
+		var row: int = int(selected_configuration[i])
+		for j in range(3):
+			var normal_stylebox: StyleBoxFlat = StyleBoxFlat.new()
+			normal_stylebox.bg_color = colors[(row-1) * 3 + j]
+			print(colors[i])
 
+			buttons[i * 3 + j].add_theme_stylebox_override("normal", normal_stylebox)
 	for i in range(len(buttons)):
-		var normal_stylebox: StyleBoxFlat = StyleBoxFlat.new()
+		buttons[i].pressed.connect(on_button_pressed.bind(i))
 
-		normal_stylebox.bg_color = colors[i]
-		print(colors[i])
+	accept_button.pressed.connect(check_sequence)
+	cancel_button.pressed.connect(cancel_last_move)
 
-		buttons[i].add_theme_stylebox_override("normal", normal_stylebox)
-
-	answer_label.text = answer
-
+func _process(delta):
+	if len(entered_sequence) == 6:
+		accept_button.disabled = false
+	else:
+		accept_button.disabled = true
+	answer_label.text = ""
+	for i in range(len(entered_sequence)):
+		answer_label.text += "X"
 
 func on_button_pressed(number: int):
-	pass
+
+	if len(entered_sequence) < 6:
+		entered_sequence.append(number)
+	else:
+		pass # DODAĆ ERROR DŹWIĘK!!!
+	
+
+func cancel_last_move():
+	entered_sequence.pop_back()
+
+
+func check_sequence():
+	if entered_sequence == configurations[selected_configuration]:
+		finished.emit(true)
+	else:
+		finished.emit(false)
