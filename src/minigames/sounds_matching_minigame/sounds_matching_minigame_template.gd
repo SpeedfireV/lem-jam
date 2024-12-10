29bb19a077ff0eca947ecc7a76a627ee6f8cc522
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-12-10 13:31:59 +0100

    sound minigame working

---
+++ src/minigames/sounds_matching_minigame/sounds_matching_minigame_template.gd
@@ -0,0 +1,18 @@
+extends MinigameViewTemplate
+
+@onready var minigame = $BackgroundTextureRect/MarginContainer/Mingame/SoundsMatchingMinigame
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super._ready()
+	minigame.finished.connect(minigame_finished)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func win():
+	super.win()
+
+func lose():
+	super.lose()
