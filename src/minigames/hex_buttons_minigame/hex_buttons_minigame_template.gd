d591fc610514ce62e303065e98cdd99131763e48
WITEX121 <witexplaygames@gmail.com>
2024-12-10 14:19:54 +0100

    adsfklh

--- src/minigames/hex_buttons_minigame/hex_buttons_minigame_template.gd
+++ src/minigames/hex_buttons_minigame/hex_buttons_minigame_template.gd
@@ -5,10 +5,3 @@ extends MinigameViewTemplate
 func _ready():
 	super._ready()
 	hex_minigame_node.finished.connect(minigame_finished)
-
-func win(): # Nadpisuje bazową wygraną
-	ScenesManager.minigames_node.clear_minigames()
-	# TODO: Ogarnąć zapis zrobionych minigier
-
-func lose(): # Nadpisuje bazową przegraną
-	ScenesManager.minigames_node.clear_minigames()
