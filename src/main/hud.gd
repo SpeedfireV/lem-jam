ee11ba99df7cbfb22d7edeaff6120104ec4fd91a
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-12-10 07:55:59 +0100

    Added timer, working on time minigame

--- src/main/hud.gd
+++ src/main/hud.gd
@@ -1,10 +1,23 @@
 class_name Hud extends Node
 
 @onready var hud_canvas_layer: CanvasLayer = $HudCanvasLayer
+@onready var timer: Timer = $Timer
+@onready var time_label: Label = $CanvasLayer/TimeLabel
 
 func _ready():
 	ScenesManager.hud = self
 
+func _process(delta):
+	time_label.text = str(seconds_to_mm_ss(int(timer.time_left)))
 
 func add_info(label: Label):
 	hud_canvas_layer.add_child(label)
+
+func clear_texts():
+	for child in hud_canvas_layer.get_children():
+		child.queue_free()
+
+func seconds_to_mm_ss(seconds: int) -> String:
+	var minutes = seconds / 60  # Get the number of whole minutes
+	var remaining_seconds = seconds % 60  # Get the remaining seconds
+	return "%02d:%02d" % [minutes, remaining_seconds]  # Format as MM:SS
--- src/minigames/color_time_minigame/color_time_minigame.gd
+++ src/minigames/color_time_minigame/color_time_minigame.gd
@@ -1,15 +1,13 @@
-extends InterationAreaTemplate
+class_name ColorTimeMinigame extends Control
 
+
+@onready var button = $HBoxContainer2/VBoxContainer/Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	super._ready()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func player_entered_area(body: Node2D):
-	super.player_entered_area(body)
-	
